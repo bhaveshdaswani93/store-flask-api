232bf86142f10d752b7225216668d12151b82829
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2024-10-08 21:04:46 +0530

    Add store data and endpoint to retrieve stores

--- app.py
+++ app.py
@@ -4,6 +4,16 @@ print(__name__)
 
 app = Flask(__name__)
 
-@app.get('/')
-def hello():
-  return "Hello world"
+stores = [{
+  'name': 'My Wonderful Store',
+  'items': [{
+    'name': 'My Item',
+    'price': 15.99
+  }]
+}]
+
+@app.get('/store')
+def get_stores():
+  return {
+    'stores': stores
+  }

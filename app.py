506f057ebac9b07642fc2e7c3859c2e9f3655a50
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2024-10-12 14:06:11 +0530

    working on get all item and get single item api

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def get_stores():
 @app.post('/store')
 def create_store():
   req_body = request.get_json()
-  store_id = uuid.uuidv4().hex
+  store_id = uuid.uuid4().hex
   new_store = {**req_body, 'store_id': store_id }
   stores[store_id] = new_store
   return new_store, 201
@@ -33,22 +33,32 @@ def create_store():
 @app.post('/item')
 def create_item():
   req_body = request.get_json()
+  store_id = req_body['store_id']
+
+  if store_id not in stores.keys():
+    return {'message': "store not found"}, 404
+
   item_id = uuid.uuid4().hex
   new_item = {**req_body, 'item_id': item_id}
   items[item_id] = new_item
   return new_item, 201
-  
-  @app.get('/store/<string:store_id>')
-  def get_store(store_id):
-    try:
-      return {'store': stores[store_id]}
-    except KeyError:
-      return {'message': 'Store not found'}, 404
-  
-  @app.get('/store/<string:name>/items')
-  def get_items_in_store(name):
-    for store in stores:
-      if store['name'] == name:
-        return {'items': store['items']}
-    return {'message': 'store not found'}, 404
+
+@app.get('/item')
+def get_all_items():
+  return {'items': list(items.values())}
+
+@app.get('/store/<string:store_id>')
+def get_store(store_id):
+  try:
+    return {'store': stores[store_id]}
+  except KeyError:
+    return {'message': 'Store not found'}, 404
+
+@app.get('/item/<string:item_id>')
+def get_item(item_id):
+  try:
+    item = items[item_id]
+    return {'item': item}
+  except KeyError:
+    return {'message': 'item not found'}, 404
   

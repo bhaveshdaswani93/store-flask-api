5f6fdad8fddecdeb30c964e04257d693091ffcc1
Bhavesh <bhaveshdaswani93@gmail.com>
2024-10-12 13:47:06 +0530

    working create item api changes

--- app.py
+++ app.py
@@ -25,33 +25,30 @@ def get_stores():
 @app.post('/store')
 def create_store():
   req_body = request.get_json()
-  store_id = uuid.uuid4().hex
+  store_id = uuid.uuidv4().hex
   new_store = {**req_body, 'store_id': store_id }
   stores[store_id] = new_store
   return new_store, 201
 
-@app.post('/store/<string:name>/item')
-def create_item_in_store(name):
+@app.post('/item')
+def create_item():
   req_body = request.get_json()
-  for store in stores:
-    if store['name'] == name:
-      new_item = {'name': req_body['name'], 'price': req_body['price']}
-      store['items'].append(new_item)
-      return new_item, 201
-  return "Store not found", 404
+  item_id = uuid.uuid4().hex
+  new_item = {**req_body, 'item_id': item_id}
+  items[item_id] = new_item
+  return new_item, 201
   
-@app.get('/store/<string:store_id>')
-def get_store(store_id):
-  try:
-    return {'store': stores[store_id]}
-  except KeyError:
-    return {'message': 'Store not found'}, 404
-
-@app.get('/store/<string:name>/items')
-def get_items_in_store(name):
-  for store in stores:
-    if store['name'] == name:
-      return {'items': store['items']}
-  return {'message': 'store not found'}, 404
-
+  @app.get('/store/<string:store_id>')
+  def get_store(store_id):
+    try:
+      return {'store': stores[store_id]}
+    except KeyError:
+      return {'message': 'Store not found'}, 404
+  
+  @app.get('/store/<string:name>/items')
+  def get_items_in_store(name):
+    for store in stores:
+      if store['name'] == name:
+        return {'items': store['items']}
+    return {'message': 'store not found'}, 404
   

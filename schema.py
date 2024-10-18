90057225f0efab5e50cb38da00fea549eeca2268
Bhavesh <bhaveshdaswani93@gmail.com>
2024-10-18 20:54:20 +0530

    creating item update schema

--- schema.py
+++ schema.py
@@ -1,1 +1,11 @@
 from marshmellow import Schema, fields
+
+class ItemSchema(Schema):
+  id = fields.Str(dump_only=True)
+  name = fields.Str(required=True)
+  price = fields.float(required=True)
+  store_id = fields.Str(required=True)
+
+class ItemUpdateSchema(Schema):
+  name = fields.Str()
+  price = fields.float()

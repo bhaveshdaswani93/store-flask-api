c5d3db06d8a2ba8ac1d68711423fcf85a44de41e
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2024-10-19 14:51:49 +0530

    Refactor item and store resources

--- resources/item.py
+++ resources/item.py
@@ -1,8 +1,9 @@
 import uuid
 from flask import request
 from flask.views import MethodView
-from flask_smorest import Blueprint
-from schemas import ItemSchema, ItemUpdateSchema
+from flask_smorest import Blueprint, abort
+from schema import ItemSchema, ItemUpdateSchema
+from db import items, stores
 
 blp = Blueprint('Items', __name__, description="Item Operation")
 
@@ -23,8 +24,8 @@ class Item(MethodView):
     except KeyError:
      abort(404, 'Item not found')
     
- @blp.arguments(ItemUpdateSchema)
- @blp.response(200, ItemSchema)
+  @blp.arguments(ItemUpdateSchema)
+  @blp.response(200, ItemSchema)
   def put(self, req_body, item_id):
     #req_body = request.get_json()
     try:
@@ -36,23 +37,25 @@ class Item(MethodView):
       abort(404, message='item not found')
       
   class ItemList(MethodView):
+    @blp.response(ItemSchema(many=True))
     def get(self):
-      return {'items': list(items.values())}
+      return items.values()
     
     @blp.arguments(ItemSchema)
+    @blp.response(201, ItemSchema)
     def post(self, req_body):
      # req_body = request.get_json()
   
-  for item in items.values():
-    if (item['name'] == req_body['name'] and item['store_id'] == req_body['store_id']):
-      abort(400, message='Item already exists')
-  
-  store_id = req_body['store_id']
+      for item in items.values():
+        if (item['name'] == req_body['name'] and item['store_id'] == req_body['store_id']):
+          abort(400, message='Item already exists')
+      
+      store_id = req_body['store_id']
 
-  if store_id not in stores.keys():
-    abort(404, message='Store not found')
+      if store_id not in stores.keys():
+        abort(404, message='Store not found')
 
-  item_id = uuid.uuid4().hex
-  new_item = {**req_body, 'item_id': item_id}
-  items[item_id] = new_item
-  return new_item, 201
+      item_id = uuid.uuid4().hex
+      new_item = {**req_body, 'item_id': item_id}
+      items[item_id] = new_item
+      return new_item, 201
--- resources/store.py
+++ resources/store.py
@@ -4,15 +4,16 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import stores
-from schemas import StoreSchema
+from schema import StoreSchema
 
 blp = Blueprint('Stores', __name__, description='Operations on stores')
 
 @blp.route('/store/<string:store_id>')
 class Store(MethodView):
+  @blp.response(200, StoreSchema)
   def get(self, store_id):
     try:
-     return {'store': stores[store_id]}
+     return stores[store_id]
     except KeyError:
      abort(404, message='Store not found')
   
@@ -23,23 +24,24 @@ class Store(MethodView):
     except KeyError:
      abort(404, message='Store not found')
   
+  @blp.response(200, StoreSchema)
   def put(self, store_id):
     try:
       req_body = request.get_json()
       store = stores[store_id]
       store |= req_body
-      return {'store': store}
+      return store
     except KeyError:
       abort(404, message="Store not found")
 
 @blp.route('/store')
 class StoreList(MethodView):
+  @blp.response(200, StoreSchema(many=True))
   def get(self):
-    return {
-      'stores': list(stores.values())
-    }
+    return stores.values()
   
   @blp.arguments(StoreSchema)
+  @blp.response(201, StoreSchema)
   def post(self, req_body):
     #req_body = request.get_json()
 

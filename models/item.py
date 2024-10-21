d86c7d0131d4fd72987dd4a489ec3e1dd70212ea
Bhavesh <bhaveshdaswani93@gmail.com>
2024-10-21 13:40:47 +0530

    workimg on store model

--- models/item.py
+++ models/item.py
@@ -2,3 +2,8 @@ from db import db
 
 class ItemModel(db.Model):
   __tablename__ = "items"
+  
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Columb(db.String(80), unique=True, nullable=False)
+  price = db.Column(db.float(precision= 2), unique=False, nullable=False)
+  store_id = db.Column(db.Integer, unique=False, nullable=False)

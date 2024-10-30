d19eb3b92d4c059e0ced3dccd7d397f650cee4aa
Bhavesh <bhaveshdaswani93@gmail.com>
2024-10-30 13:38:51 +0530

    working on tags and items relationship wip

--- models/item_tags.py
+++ models/item_tags.py
@@ -4,3 +4,5 @@ class ItemTags(db.Model):
   __tablename__ = "items_tags"
 
   id = db.Column(db.Integer, primary_key=True)
+  item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+  tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'))

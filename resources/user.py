4a06dea249d4e9c6d9655a08068e8a3815783fff
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2024-11-04 14:05:03 +0530

    Fix user registration error handling and correct variable name

--- resources/user.py
+++ resources/user.py
@@ -1,5 +1,5 @@
 from flask.views import MethodView
-from flask_smorest import Blueprint
+from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
 
 from db import db
@@ -13,16 +13,16 @@ class UserRegister(MethodView):
   @blp.arguments(UserSchema)
   def post(self, user_data):
     if UserModel.query.filter(UserModel.username == user_data['username']).first():
-      abort(400, message=f'User with username {user_data['username']} already exists')
+      abort(400, message=f'User with username {user_data["username"]} already exists')
     
     user = UserModel(
-        username = uaer_data['username'],
+        username = user_data['username'],
         password = pbkdf2_sha256.hash(user_data['password'])
       )
       
-      db.session.add(user)
-      db.session.commit()
+    db.session.add(user)
+    db.session.commit()
       
-      return {'message': 'User created successfully'}, 201
+    return {'message': 'User created successfully'}, 201
     
 

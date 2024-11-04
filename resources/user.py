3d10f7ca9c84b4edfca143b03e294812deabbb01
Bhavesh <bhaveshdaswani93@gmail.com>
2024-11-04 13:42:09 +0530

    working on register user api

--- resources/user.py
+++ resources/user.py
@@ -8,3 +8,21 @@ from schema import UserSchema
 
 blp = Blueprint('Users', 'users', description="Operations on user")
 
+@blp.route('/register')
+class UserRegister(MethodView):
+  @blp.arguments(UserSchema)
+  def post(self, user_data):
+    if UserModel.query.filter(UserModel.username == user_data['username']).first():
+      abort(400, message=f'User with username {user_data['username']} already exists')
+    
+    user = UserModel(
+        username = uaer_data['username'],
+        password = pbkdf2_sha256.hash(user_data['password'])
+      )
+      
+      db.session.add(user)
+      db.session.commit()
+      
+      return {'message': 'User created successfully'}, 201
+    
+
